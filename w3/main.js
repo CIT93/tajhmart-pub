cab4650212dbf344beae2cd800091c381990051c
t3true <truedesigner@outlook.com>
2024-02-01 03:46:44 -0800

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,23 +1,30 @@
-const myHeading = document.querySelector("h3");
 
-let carbonFootprintPoints = 0;
-const numberInHousehold = 3;
+function determineHouseHoldPts(numberInHousehold) {
 
-if (numberInHousehold === 1) {
-    carbonFootprintPoints = carbonFootprintPoints + 14;
-} else if(numberInHousehold === 2) {
-    carbonFootprintPoints = carbonFootprintPoints + 12;
-} else if(numberInHousehold === 3) {
-    carbonFootprintPoints = carbonFootprintPoints + 10;
-} else if(numberInHousehold === 4) {
-    carbonFootprintPoints = carbonFootprintPoints + 8;
-} else if(numberInHousehold === 5) {
-    carbonFootprintPoints = carbonFootprintPoints + 6;
-} else if(numberInHousehold === 6) {
-    carbonFootprintPoints = carbonFootprintPoints + 4;
-} else if (numberInHousehold > 6){
-    carbonFootprintPoints = carbonFootprintPoints +2;
-}
+    const myHeading = document.querySelector("h3");
+
+ if (numberInHousehold === 1) {
+   carbonFootprintPoints = carbonFootprintPoints + 14;
+ } else if (numberInHousehold === 2) {
+   carbonFootprintPoints = carbonFootprintPoints + 12;
+ } else if (numberInHousehold === 3) {
+   carbonFootprintPoints = carbonFootprintPoints + 10;
+ } else if (numberInHousehold === 4) {
+   carbonFootprintPoints = carbonFootprintPoints + 8;
+ } else if (numberInHousehold === 5) {
+   carbonFootprintPoints = carbonFootprintPoints + 6;
+ } else if (numberInHousehold === 6) {
+   carbonFootprintPoints = carbonFootprintPoints + 4;
+ } else if (numberInHousehold > 6) {
+   carbonFootprintPoints = carbonFootprintPoints + 2;
+ }
 
 const outputString = ("Based on the number of members of the household of " + numberInHousehold + ", the points would be " + carbonFootprintPoints)
-myHeading.textContent = outputString;
+myHeading.textContent = outputString;   
+}
+
+let carbonFootprintPoints = 0;
+//const numberInHousehold = 3;
+
+determineHouseHoldPts(2)
+determineHouseHoldPts(1)
